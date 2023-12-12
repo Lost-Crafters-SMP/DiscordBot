31a4f754ad0d76b106d22e597abf1e51abb6c4ef
FireBall1725 <727458+FireBall1725@users.noreply.github.com>
2023-12-11 21:51:56 -0500

    license

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,9 @@
+/*
+ * Created for the Lost Crafters SMP (https://www.lostcrafterssmp.com)
+ * Licensed under the GNU Affero General Public License v3.0
+ * See LICENSE.txt for full license information
+ */
+
 group = "ca.fireball1725.lcs.discordbot"
 version = "0.1.0"
 description = "Lost Crafters SMP Discord Bot"
--- app/src/main/kotlin/ca/fireball1725/lcs/discordbot/helpers/ServerHelper.kt
+++ app/src/main/kotlin/ca/fireball1725/lcs/discordbot/helpers/ServerHelper.kt
@@ -1,3 +1,9 @@
+/*
+ * Created for the Lost Crafters SMP (https://www.lostcrafterssmp.com)
+ * Licensed under the GNU Affero General Public License v3.0
+ * See LICENSE.txt for full license information
+ */
+
 package ca.fireball1725.lcs.discordbot.helpers
 
 import ca.fireball1725.lcs.discordbot.getServers
